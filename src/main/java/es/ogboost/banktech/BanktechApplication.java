973d0d85912f147a08f4c0abd8669242f84afcee
Sergio Sanchez <sergiosg140@gmail.com>
2025-11-01 18:42:32 +0100

    Initial commit

---
+++ src/main/java/es/ogboost/banktech/BanktechApplication.java
@@ -0,0 +1,13 @@
+package es.ogboost.banktech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanktechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanktechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/ogboost/banktech/BanktechApplicationTests.java
@@ -0,0 +1,13 @@
+package es.ogboost.banktech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanktechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
